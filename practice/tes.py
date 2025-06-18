b20571be395b938910ee6492fcfe009db3bacb2d
Fabiola701 <waleanfabiola@gmail.com>
2025-06-18 20:05:59 +0800

    first commit

---
+++ practice/tes.py
@@ -0,0 +1,19 @@
+print("hello")
+print(5+5)
+print("hello, World!")
+import sys
+print(sys.version)
+if 5 > 2:
+    print("Five is greater than two!")
+x = 5 
+y = "Hello, World!"
+#This is a comment.
+print ("Hello, World")
+#print("Hello World!")
+print("Cheers, Mate!")
+"""
+This is a comment
+written in
+more than just one line
+"""
+
